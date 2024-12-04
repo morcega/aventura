3cf62f09f12da8984c1b36bf6b8e274c2597de7f
Batman <maria.cruz.alves29@escola.pr.gov.br>
2024-12-04 08:22:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const avanca = document.querySelectorAll('.btn-ptoximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('ativo');
+        const proximoPasso ='passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+document.getElementById(proximoPasso).classList.add('ativo');
+
+
+
+
+    })
+})
